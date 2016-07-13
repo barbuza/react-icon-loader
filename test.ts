f6fb297253228cef6e87de4479a9fbddd2a24fd0
Victor Kotseruba <barbuzaster@gmail.com>
2016-07-13 14:11:47 -0700

    gardening

--- test.ts
+++ test.ts
@@ -27,22 +27,36 @@ function xmlEqual(a: string, b: string): boolean {
   return _.isEqual(ta, tb);
 }
 
-tape('stress', (t: tape.Test) => {
+tape('stress', t => {
+
+  const testFile = async.compose(
+    (filename, optimized, js, callback) => {
+      const comp = eval(js);
+      t.equal(comp.displayName, path.basename(filename), `displayName ${path.basename(filename)}`);
+      const reactXml = ReactDOMServer.renderToStaticMarkup(React.createElement(comp));
+      t.ok(xmlEqual(reactXml, optimized), `render ${path.basename(filename)}`);
+      callback();
+    },
+
+    (filename, source, optimized, callback) => {
+      loader.call({
+        resourcePath: filename,
+        async: () => (err, js) => callback(err, filename, optimized, js)
+      }, source);
+    },
+
+    (filename, source, callback) => {
+      const svgo = new SVGO(cleanupOpts);
+      svgo.optimize(source, result => callback(null, filename, source, result.data));
+    },
+
+    (filename, callback) => {
+      fs.readFile(filename, 'utf-8', (err, source) => callback(err, filename, source));
+    }
+  ) as AsyncIterator<string>;
+
   glob('node_modules/material-design-icons/**/svg/production/*.svg', null, (err, files) => {
-    async.each(files, (filename, callback) => {
-      fs.readFile(filename, 'utf-8', (err, source) => {
-        const svgo = new SVGO(cleanupOpts);
-        svgo.optimize(source, result => {
-          function cb(err, js) {
-            const comp = eval(js);
-            t.equal(comp.displayName, path.basename(filename));
-            const reactXml = ReactDOMServer.renderToStaticMarkup(React.createElement(comp));
-            t.ok(xmlEqual(reactXml, result.data), filename);
-            callback();
-          }
-          loader.call({ resourcePath: filename, async: () => cb }, source);
-        });
-      });
-    }, t.end);
+    async.eachLimit(files, 4, testFile, t.end);
   });
+
 });
